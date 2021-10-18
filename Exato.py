139f3679e6982b09b3c6fe3ab9923f7249a07cd9
Emerson Antunes <eoantunes@gmail.com>
2021-10-18 17:03:01 -0300

    Inclusão da variável de decisão para controle da Associação entre clientes e eNodeBs.

--- Exato.py
+++ Exato.py
@@ -8,27 +8,6 @@ I,J = 12,15
 def m2a(i,j,line_length):
     return i*line_length + j
 
-def maior(X):
-    if 5 in X:
-        return 5
-    elif 4 in X:
-        return 4
-    elif 3 in X:
-        return 3
-    elif 2 in X:
-        return 2
-    elif 1 in X:
-        return 1
-    else:
-        return 0
-
-def multVector(a,b):
-    if len(a) == len(b):
-        aux = []
-        for i in range(len(a)):
-            aux.append(a[i]*b[i])
-    return aux
-
 # Declaração do solver ORTools considerando a biblioteca CBC do Google
 solver = pywraplp.Solver("Exato", pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
 
@@ -74,8 +53,8 @@ M = Smnp.shape[0]  # Nr de pontos de demanda
 N = Smnp.shape[1]  # Nr de possíveis locais para instalação de eNodeBs
 P = Smnp.shape[2]  # Nr de potências sendo avaliadas
 Ant = 9  # Nr máximo de antenas (No CCOp Mv o número máximo é 9 = 8 Vtr Nó de acesso + 1 Centro de Coordenação)
-Usu = 200  # Nr máximo de usuários associados a uma eNodeB
-Interc = 4 # Nr mínimo de nós interconectados
+Usu = 100  # Nr máximo de usuários associados a uma eNodeB
+Interc = 1 # Nr mínimo de nós interconectados
 
 
 
@@ -91,6 +70,11 @@ Y = []  # eNodeBs
 for m in range(0, M):
     for p in range(0, P):
         Y.append(solver.BoolVar('Y[%d,%d]'%(m,p)))
+
+Z = []  # Associação de quadrículas de clientes à eNodeBs --> Zmn = 1, se a quadrícula n for designada para ser atendida pela eNodeB m
+for m in range(0, M):
+    for n in range(0, N):
+        Z.append(solver.BoolVar('Z[%d,%d]'%(m,n)))
 
 ##############
 # Restrições #
@@ -108,13 +92,28 @@ for n in range(0, N):
             ct.SetCoefficient(Y[m2a(m, p, P)], int(Cmnp[m][n][p]))
 
 # Restrição (2): Número de usuários por eNodeB deve ser menor ou igual a Usu
+#for m in range(0, M):
+#    ct = solver.Constraint(0, Usu, str(head))
+#    head += 1
+#    for n in range(0, N):
+#        for p in range(0, P):
+            #ct.SetCoefficient(X[n]*Y[m2a(m, p, P)], int(Cmnp[m][n][p]))
+#            ct.SetCoefficient(X[n], int(Cmnp[m][n][p] * Nij[nn[n][0]][nn[n][1]]))
+
+# Cada quadrícula de cliente Nij só pode ser atendida ou associada a uma eNodeB
+for n in range(0, N):
+    ct = solver.Constraint(0, 0, str(head))
+    ct.SetCoefficient(X[n], -1)
+    head += 1
+    for m in range(0, M):
+        ct.SetCoefficient(Z[m2a(m, n, N)], 1)
+
+# Cada eNodeB não pode atender ou ter mais clientes associados que sua capacidade máxima = Usu
 for m in range(0, M):
     ct = solver.Constraint(0, Usu, str(head))
     head += 1
     for n in range(0, N):
-        for p in range(0, P):
-            #ct.SetCoefficient(X[n]*Y[m2a(m, p, P)], int(Cmnp[m][n][p]))
-            ct.SetCoefficient(X[n], int(Cmnp[m][n][p]))
+        ct.SetCoefficient(Z[m2a(m, n, N)], Nij[nn[n][0]][nn[n][1]])
 
 
 # Restrição (3): Número máximo de antenas instaladas ==> Somatório (mEM) Somatório (pEP) ymp <= Ant
@@ -125,13 +124,14 @@ for m in range(0, M):
 head += 1
 
 # Restrição (4): Número mínimo de antenas interconectadas - ROA 65
-for k in range(0, M):
-    for z in range(0, P):
+for m in range(0, M):
+    for p in range(0, P):
         ct = solver.Constraint(Interc, Ant, str(head))
+        ct.SetCoefficient(Y[m2a(m, p, P)], -1)
         head += 1
-        for m in range(0, M):
-            for p in range(0, P):
-                ct.SetCoefficient(Y[m2a(m, p, P)], int(A[m][k][p][z]))
+        for k in range(0, M):
+            for z in range(0, P):
+                ct.SetCoefficient(Y[m2a(k, z, P)], int(A[m][k][p][z]))
 
 # Restrição (5): Uma eNodeB só pode ter uma potência de transmissão (pTx) ativada
 for m in range(0, M):
@@ -149,7 +149,7 @@ objetivo = solver.Objective()
 # Maximização do número de clientes atendidos ou cobertura
 for n in range(0, N):
     #objetivo.SetCoefficient(X[n], maior(multVector(Y, C[i])) * Nij[nn[n][0]][nn[n][1]]) # Voltar para analisar a função top
-    objetivo.SetCoefficient(X[n], Nij[nn[n][0]][nn[n][1]])
+    objetivo.SetCoefficient(X[n], int(Nij[nn[n][0]][nn[n][1]]))
 
 # Minimização do número de eNodeBs empregadas
 for m in range (0, M):
@@ -163,8 +163,13 @@ for m in range(0, M):
             objetivo.SetCoefficient(Y[m2a(m, p, P)], -1 * Cmnp[m][n][p])
 for n in range(0, N):
     objetivo.SetCoefficient(X[n], 1)
-objetivo.SetMaximization()
 
+# Minimização do custo de associação
+#for m in range(0, M):
+#    for n in range(0, N):
+#        objetivo.SetCoefficient(Z[m2a(m, n, N)], -1)
+
+objetivo.SetMaximization()
 solver.Solve()
 depois = time.time()
 
@@ -198,4 +203,18 @@ for m in range(0, M):
 print(str(int(NrAntenasInstaladas)) + ' eNodeBs instalados.')
 
 print()
-print('Tempo gasto no processamento = ' + str(depois - antes))
+print("Associações de quadrículas de eNodeBs Mij e quadrículas de clientes Nij:")
+for m in range(0, M):
+    for n in range(0, N):
+        if Z[m2a(m, n, N)] == 1:
+            print(Z[m2a(m, n, N)])
+        else:
+            print("Opa, apareceu Zmn = 0!")
+
+
+print()
+print('Tempo gasto no processamento (time) = ' + str(depois - antes))
+print("Tempo de processamento = %f" % solver.wall_time())
+print("Iterações: %d" % solver.iterations())
+print("Problema resolvido usando %d branch-and-bound nós" % solver.nodes())
+
